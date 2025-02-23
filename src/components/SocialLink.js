7fe6e86e2c3d9f8e266f10eaed15964a4bbc08fb
Rodrigo Hashimoto <rodhash@gmail.com>
2025-02-23 09:22:14 -0300

    fix: links security flaw
    Signed-off-by: Rodrigo Hashimoto <rodhash@gmail.com>

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialLink = ({href, icon, className}) => {
   return (
     <li>
-      <a href={href} target="_blank" className={className}
+      <a href={href} target="_blank" rel="noreferrer" className={className}
         ><i className={icon}></i
       ></a>
     </li>
