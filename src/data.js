8fb564af43af805b06dc279becc3ce68cd647974
Rodrigo Hashimoto <rodhash@gmail.com>
2025-02-23 09:20:03 -0300

    fix: unused vars
    Signed-off-by: Rodrigo Hashimoto <rodhash@gmail.com>

--- src/data.js
+++ src/data.js
@@ -3,8 +3,6 @@ import img1 from './images/tour-1.jpeg'
 import img2 from './images/tour-2.jpeg'
 import img3 from './images/tour-3.jpeg'
 import img4 from './images/tour-4.jpeg'
-import img5 from './images/tour-5.jpeg'
-import img6 from './images/tour-6.jpeg'
 
 export const PageLinks = [
   {id: 1, href: '#home', text: 'home'},
